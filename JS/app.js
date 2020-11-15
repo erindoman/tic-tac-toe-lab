cf25a6c0607a9f1017952883bcda3e61a9b84d57
erindoman <erin.doman@gmail.com>
2020-11-14 20:37:36 -0600

    update clickable squares and depricated event

--- JS/app.js
+++ JS/app.js
@@ -9,58 +9,57 @@ const wins = [
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8]
-]
+];
 
 const messages = document.querySelector('#message')
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let currentPlayer = "X";
-let newGame = true;
+let turn = "X";
 let winner;
 let board = ["", "", "", "", "", "", "", "", ""];
-const messageText = document.getElementById('h2')
 
 /*------------------------ Cached Element References ------------------------*/
 
-const squares = Array.from(document.querySelectorAll('#board div'))
+const squares = document.querySelectorAll('#board div')
+const messageText = document.getElementById('h2')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 document.getElementById('board').addEventListener('click', playerTurn)
 document.querySelector('.resetButton').addEventListener('click', init);
 
+
 /*-------------------------------- Functions --------------------------------*/
 
 function getWinner() {
     let winner = null;
-    wins.forEach(function(win, index) {
+    wins.forEach(function(win) {
         if (board[win[0]] && board[win[0]] === board[win[1]] && board[win[0]] === board[win[2]]) winner = board[win[0]];
     })
     return winner ? winner : board.includes("") ? null : "tie";    
 };
 
-function playerTurn() {
-    let idx = squares.findIndex(function(square) {
-        return square === event.target; //event is deprecated?
-    });
-    board[idx] = currentPlayer;
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
+function playerTurn(e) {
+    let idx = parseInt(e.target.id.replace('sq', '')); 
+    if (board[idx] || winner) return;
+    board[idx] = turn;
+    turn = turn === "X" ? "O" : "X";
     winner = getWinner();
     render();
 }
 
+function render() {
+    board.forEach(function(mark, idx) {
+        squares[idx].textContent = mark;
+    });
+    messages.textContent = winner === "tie" ? "It's a tie!" : winner ? `${winner} wins!` : `${turn}'s turn!`
+}
+
 function init() {
     board = ["", "", "", "", "", "", "", "", ""];
-    const messageText = document.getElementById('h2')
+    winner = null;
     render();
 };
-
-function render() {
-    board.forEach(function(mark, index) {
-        squares[index].textContent = mark;
-    });
-    messages.textContent = winner === "tie" ? "It's a tie!" : winner ? `${winner} wins!` : `${currentPlayer}'s turn!`
-}
 
 init();
